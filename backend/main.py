f3f1e3f644f8b72b5afacdcdef34bef066241671
vectorthrust <182170925+vectorthrust@users.noreply.github.com>
2025-05-31 20:58:39 -0400

    feat: agent templates and cleanup

---
+++ backend/main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, WebSocket
+from langchain_openai import ChatOpenAI
+from browser_use import Agent, ActionResult, Controller
+from dotenv import load_dotenv
+import uvicorn
+import asyncio
+import json
+from templates import foodTemplate, flightTemplate
+
+load_dotenv()
+
+app = FastAPI()
+llm = ChatOpenAI(model="gpt-4o")
+
+@app.get("/ping")
+async def ping():
+    return {"message": "Server is alive!"}
+
+@app.websocket("/ws/agent")
+async def websocket_agent(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        initialData = await websocket.receive_text()
+        data = json.loads(initialData)
+
+        async def step_hook(agent: Agent):
+            agentOutputs = agent.state.history.model_outputs()
+            if agentOutputs:
+                latestOutput = agentOutputs[-1]
+                stepUpdate = {
+                    "next_goal": latestOutput.current_state.next_goal
+                }
+                await websocket.send_text(json.dumps(stepUpdate))
+
+        formattedTask = ""
+        
+        if data["taskType"] == "food":
+            address = data["details"]["address"]
+            restaurantName = data["details"]["restaurantName"]
+            item = data["details"]["item"]
+
+            formattedTask = foodTemplate(address, restaurantName, item)
+        if data["taskType"] == "flight":
+
+            formattedTask = flightTemplate()
+
+        agent = Agent(
+        task=formattedTask,
+        llm=llm,
+        )
+
+        result = await agent.run(
+        on_step_start=step_hook
+        )
+
+        visitedLog= agent.state.history.urls()
+
+        lastLink = visitedLog[-1] if visitedLog else None
+
+        await websocket.send_text(json.dumps({
+            "status": "done",
+            "result": lastLink
+        }))
+
+        await websocket.close()
+            
+    except Exception as e:
+        print("Error:", e)
+        await websocket.close()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
---
+++ backend/templates.py
@@ -0,0 +1,55 @@
+def template(taskType):
+    print("jah")
+#    elif taskType == "flight":
+
+ #   elif taskType == "taxi":
+
+  #  elif taskType == "amazon":
+
+def foodTemplate(address, restaurantName, item):
+
+     task=f'''
+        1. Go to Wolt.com
+        2. Click the location blip icon
+        3. Click Add new address
+        4. In the Street name and number input enter {address} and then click Enter
+        5. Click Continue
+        6. On the location selection click on the Chose button for Other the fourth option
+        7. In the Address details box enter {address} and then press enter
+        8. Click on the Search in Wolt... search bar
+        9. Enter {restaurantName} and then search (press enter)
+        10. From the available options, identify the option in the list that has the text {restaurantName} then click it
+        11. Scroll through this page until you find the {item} and then click on it
+        12. Click Add to order
+        13. Click View Order
+        14. Click Go to checkout
+        15. Now find and click the Click to order button
+        16. Click Share tracking
+        17. Save the entire track.wolt.com url visible on screen and then navigate to it
+        '''
+     
+     return task
+
+def flightTemplate():
+
+     task=f'''
+        1. Go to Wolt.com
+        2. Click the location blip icon
+        3. Click Add new address
+        4. In the Street name and number input enter {address} and then click Enter
+        5. Click Continue
+        6. On the location selection click on the Chose button for Other the fourth option
+        7. In the Address details box enter {address} and then press enter
+        8. Click on the Search in Wolt... search bar
+        9. Enter {restaurantName} and then search (press enter)
+        10. From the available options, identify the option in the list that has the text {restaurantName} then click it
+        11. Scroll through this page until you find the {item} and then click on it
+        12. Press Add to order
+        13. Press View Order
+        14. Press Go to checkout
+        '''
+     
+     return task
+
+
+
